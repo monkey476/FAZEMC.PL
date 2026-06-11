5d542bf37a42aa5f5f14e54a8987b61e75d5f256
monkey476 <stastomczuk@gmail.com>
2026-06-11 19:56:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -69,3 +69,7 @@ client.on('interactionCreate', async interaction => {
 });
 
 client.login(TOKEN);
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Bot is active!'));
+app.listen(process.env.PORT || 3000, () => console.log('Port otwarty dla Rendera!'));
